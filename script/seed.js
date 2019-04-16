eeb388bf108b18cb33ef90f43a8f15f155b7a159
Ashley Heestand <heestand.ashley@gmail.com>
2019-04-16 14:24:05 -0500

    seed and model

--- script/seed.js
+++ script/seed.js
@@ -1,7 +1,8 @@
 'use strict'
 
 const db = require('../server/db')
-const {User} = require('../server/db/models')
+const { User, Product } = require('../server/db/models')
+
 
 async function seed() {
   await db.sync({force: true})
@@ -11,10 +12,38 @@ async function seed() {
     User.create({email: 'cody@email.com', password: '123'}),
     User.create({email: 'murphy@email.com', password: '123'})
   ])
-
+const products = await Promise.all([
+  Product.create({
+    name: 'Freedom of Speach', price: 10.00, status: 'In Stock', description: 'Freedom of Speach is naturally soured in the brew kettle and fermented out dry with a lower alcohol level. Our easy-drinking session sour demonstrates a clean tartness, balanced out by an assembly of ripe peach flavor. Exercise your right to refreshment.', rating: 4.5, imageUrl: 'https://revbrew.com/imager/d37xww59oglu30_cloudfront_net/beer/FreedomofSpeach-Website_7a380b392d6d6308eb7e6af7dcd28cdd.png', inventory: 50, ABV: 4.5, brewery: 'Revolution'
+  }),
+  Product.create({
+    name: 'Freedom of Press', price: 10.00, status: 'In Stock', description: 'Keep currant with the latest entry in our session sour series-—sweet, earthy berry flavors from black currants to complement this 140 calorie, easy-drinking, lightly tart ale.', rating: 4.5, imageUrl: 'https://revbrew.com/imager/d37xww59oglu30_cloudfront_net/beer/FinalFreedomofSpeach-Website_7a380b392d6d6308eb7e6af7dcd28cdd.png', inventory: 50, ABV: 4.5, brewery: 'Revolution'
+  }),
+  Product.create({
+    name: 'Rosa', price: 10.00, status: 'In Stock', description: 'A highly drinkable Golden Ale steeped with hibiscus, resulting in a natural tartness and slight pink hue.', rating: 4.8, imageUrl: 'https://revbrew.com/imager/d37xww59oglu30_cloudfront_net/beer/Rosa-WebCan_7a380b392d6d6308eb7e6af7dcd28cdd.png', inventory: 100, ABV: 5.8, brewery: 'Revolution'
+  }),
+  Product.create({
+    name: 'Sun Crusher', price: 10.00, status: 'In Stock', description: 'A crushable ale to celebrate the end of winter and rejoice the coming of warm weather!', rating: 4.2, imageUrl: 'https://revbrew.com/imager/d37xww59oglu30_cloudfront_net/beer/SunCrusher-WebCan_7a380b392d6d6308eb7e6af7dcd28cdd.png', inventory: 100, ABV: 5.3, brewery: 'Revolution'
+  }),
+  Product.create({
+    name: 'Anti-Hero', price: 10.00, status: 'In Stock', description: 'Our flagship IPA is supremely aromatic, crisp, and drinkable.', rating: 4.3, imageUrl: 'https://revbrew.com/imager/d37xww59oglu30_cloudfront_net/beer/antihero-can-NEW_7a380b392d6d6308eb7e6af7dcd28cdd.png', inventory: 200, ABV: 6.7, brewery: 'Revolution'
+  }),
+  Product.create({
+    name: 'Eugene', price: 10.00, status: 'In Stock', description: 'A striking, robust porter full of warmth and chocolate malt.', rating: 4.3, imageUrl: 'https://revbrew.com/imager/d37xww59oglu30_cloudfront_net/beer/eugene-can-NEW_7a380b392d6d6308eb7e6af7dcd28cdd.png', inventory: 200, ABV: 6.8, brewery: 'Revolution'
+  }),
+  Product.create({
+    name: 'Cross of Gold', price: 10.00, status: 'In Stock', description: 'An easy drinking golden ale for everyone.  Brewed to be crisp and refreshing with a bit of wheat malt for body and a delicate hop finish.', rating: 4.0, imageUrl: 'https://revbrew.com/imager/d37xww59oglu30_cloudfront_net/beer/COG-WebCan_7a380b392d6d6308eb7e6af7dcd28cdd.png', inventory: 100, ABV: 4.8, brewery: 'Revolution'
+  }),
+  Product.create({
+    name: 'Rev Pils', price: 10.00, status: 'In Stock', description: 'Our Chicago Pilsner. Brewed traditionally German, with a slightly American approach.', rating: 4.8, imageUrl: 'https://revbrew.com/imager/d37xww59oglu30_cloudfront_net/beer/RevPils-WebCan_7a380b392d6d6308eb7e6af7dcd28cdd.png', inventory: 200, ABV: 5.5, brewery: 'Revolution'
+  }),
+])
   console.log(`seeded ${users.length} users`)
+  console.log(`seeded ${products.length} products`)
   console.log(`seeded successfully`)
 }
+
+
 
 // We've separated the `seed` function from the `runSeed` function.
 // This way we can isolate the error handling and exit trapping.
--- server/db/models/product.js
+++ server/db/models/product.js
@@ -20,7 +20,7 @@ const Product = db.define('product', {
   status: {
     type: Sequelize.STRING,
     allowNull: false,
-    defaultValue: 'In Stock',
+    defaultValue: 'Out of Stock',
     validate: {
       notEmpty: true
     }
@@ -29,13 +29,44 @@ const Product = db.define('product', {
     type: Sequelize.TEXT,
   },
   rating: {
-    type: Sequelize.DECIMAL(10,1),
+    type: Sequelize.DECIMAL(10, 2),
     validate: {
       isDecimal: true,
-      min: 0,
-      max: 5
+      min: 0.0,
+      max: 5.0
+    }
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: 'default-beer.jpg'
+  },
+  inventory: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0
+  },
+  ABV: {
+    type: Sequelize.DECIMAL(10, 2),
+    validate: {
+      isDecimal: true,
+      min: 0.0
+    }
+  },
+  brewery: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
     }
   }
 })
+
+Product.beforeUpdate = function () {
+  if (this.inventory > 0) {
+    return this.update({
+      status: 'In Stock'
+    })
+  }
+}
 
 module.exports = Product
