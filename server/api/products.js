33ee0b2a0bf7e619b85785e4af3b62aff80cb881
Todd Ivey <toddivey@gmail.com>
2019-04-17 09:44:42 -0500

    completed product routes

---
+++ server/api/products.js
@@ -0,0 +1,69 @@
+const router = require('express').Router()
+const {Product} = require('../db/models')
+module.exports = router
+
+
+//will need to do eager loading once assosciations are set
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const data = await Product.find(
+      {where: {id: Number(req.params.productId)}
+    }
+    )
+    res.send(data)
+  } catch (err) {
+    next(err)
+  }
+})
+
+
+router.get('/', async (req, res, next) => {
+  try {
+    //will need to do eager loading once assosciations are set
+    const products = await Product.findAll()
+    res.json(products)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const product = await Product.create(req.body)
+    res.status(201)
+    .json(product)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:productId', async (req, res, next) => {
+  try {
+    await Product.update(
+      { name: req.body.name,
+        price: req.body.price,
+        status: req.body.status,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        inventory: req.body.inventory,
+        ABV: req.body.ABV,
+        brewery: req.body.brewery
+      },
+      {where: {id: req.params.productId}}
+    )
+    res.send('Product Updated!')
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/:productId', async (req, res, next) => {
+  try {
+    await Product.destroy({
+      where: {id: req.params.productId}
+    })
+    res.send('Product Deleted Successfully!')
+  } catch (err) {
+    next(err)
+  }
+})
