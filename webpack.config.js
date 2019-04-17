2d5bb9e2465eaa3946d87796043c43dba5170678
Matt Hemmerly <mhemmerly@gmail.com>
2019-04-17 10:27:06 -0500

    Added loaders to address Semantic build errors

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,19 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
-      }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+    },
+    {
+        test: /\.s[a|c]ss$/,
+        loader: 'sass-loader!style-loader!css-loader'
+    },
+    {
+        test: /\.(jpg|png|gif|jpeg|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+    }
     ]
   }
 }
