f702b208b25974108c952224ae0535c327a3bb3e
Elizabeth Silverstein <elizabeth.a.silverstein@gmail.com>
2019-04-18 10:38:17 -0500

    page is pretty now

--- client/components/products.js
+++ client/components/products.js
@@ -11,9 +11,7 @@ class AllProducts extends React.Component {
   }
   render () {
     const products = Array.from(this.props.products) || []
-    console.log("#########", this.props)
     const removeProduct = this.props.deleteProduct
-
     if (!products || products.length < 1) {
       return (
         <div>
@@ -33,12 +31,21 @@ class AllProducts extends React.Component {
                   <Card>
                 <div key={product.id}>
                     <Card.Content>
-                    <Image src={product.imageUrl} size = 'small' bordered />
-                  {/* <Link to={`/products/${product.id}`} > */}
+                    <Image src={product.imageUrl} size = 'small' />
+                  <Link to={`/products/${product.id}`}>
                     <Card.Header> {product.name}</Card.Header>
-                  {/* </Link> */}
-                  <Button type='button' onClick={() => removeProduct(product.id)}>DELETE</Button>
+                  </Link>
+                    <Card.Meta>Brewery: {product.brewery} </Card.Meta>
+                    <Card.Description>Price: ${product.price}</Card.Description>
+
                     </Card.Content>
+                    <Card.Content>
+                    <p>
+                      Alcohol Content: {product.ABV}%
+                      Status: {product.status}
+                    </p>
+                    </Card.Content>
+                    <Button className='mini ui red inverted button' onClick={() => removeProduct(product.id)}>DELETE</Button>
                 </div>
                 </Card>
                 </Grid.Column>
