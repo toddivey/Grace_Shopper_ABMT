75ce04c559b1f4ddc1ab34b8b8f85d1e3fb5d35e
Elizabeth Silverstein <elizabeth.a.silverstein@gmail.com>
2019-04-18 15:34:06 -0500

    api routes working

--- server/api/index.js
+++ server/api/index.js
@@ -5,6 +5,8 @@ router.use('/users', require('./users'))
 
 router.use('/products', require('./products') )
 
+router.use('/reviews', require('./reviews') )
+
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
