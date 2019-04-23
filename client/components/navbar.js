22d94e29cda21c287d3b2a17315b8cf839103a22
Ashley Heestand <heestand.ashley@gmail.com>
2019-04-23 16:30:42 -0500

    added image

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,76 +3,68 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link, withRouter} from 'react-router-dom'
 import {logout} from '../store'
-import {Header, Icon, Menu, Container} from 'semantic-ui-react'
+import {Header, Icon, Menu, Container, Image} from 'semantic-ui-react'
 import {me} from '../store/user'
 
-
 const Navbar = ({handleClick, isLoggedIn, user}) => {
-  console.log('user in navbar',user)
-return (
-  <nav>
-  <div>
-    <Header as="h1">
-      <Icon name="beer" />
-      <Header.Content>grace HOPper</Header.Content>
-    </Header>
-    <Menu secondary size="medium">
-    
-        {isLoggedIn ? (
-          <div>
-          <Container text>
-          <Link to="/">
-            <Menu.Item as="a">
-              {/* The navbar will show these links after you log in */}
-              Home
-            </Menu.Item>
-            </Link>
-            <Link to="/" onClick={handleClick} >
-            <Menu.Item as="a">
-              Logout
-            </Menu.Item>
-            </Link>
-            <Link to="/cart">
-            <Menu.Item as="a">CART GOES HERE</Menu.Item>
-            </Link>
-          </Container>
-          </div>
-        ) : (
-          <Container text>
-            {/* The navbar will show these links before you log in */}
-            <Link to="/">
-            <Menu.Item as="a">
-              Home
-            </Menu.Item>
-            </Link>
-            <Link to="/login">
-            <Menu.Item as="a">
-              Login
-            </Menu.Item>
-            </Link>
-            <Link to="/signup">
-            <Menu.Item as="a">
-              Sign Up
-            </Menu.Item>
-            </Link>
-            <Menu.Item as="a">
-            <Link to ="/cart">Cart</Link>
-            </Menu.Item>
-          </Container>
-        )}
-    </Menu>
-    <hr />
-  </div>
-  </nav>
-)}
+  console.log('user in navbar', user)
+  return (
+    <nav>
+      <div>
+        <Header as="h1">
+          <Icon name="beer" />
+          <Header.Content>grace HOPper</Header.Content>
+        </Header>
+        <Image src="/IMG_7360.JPG" fluid />
+        <Menu secondary size="medium">
+          {isLoggedIn ? (
+            <div>
+              <Container text>
+                <Link to="/">
+                  <Menu.Item as="a">
+                    {/* The navbar will show these links after you log in */}
+                    Home
+                  </Menu.Item>
+                </Link>
+                <Link to="/" onClick={handleClick}>
+                  <Menu.Item as="a">Logout</Menu.Item>
+                </Link>
+                <Link to="/cart">
+                  <Menu.Item as="a">CART GOES HERE</Menu.Item>
+                </Link>
+              </Container>
+            </div>
+          ) : (
+            <Container text>
+              {/* The navbar will show these links before you log in */}
+              <Link to="/">
+                <Menu.Item as="a">Home</Menu.Item>
+              </Link>
+              <Link to="/login">
+                <Menu.Item as="a">Login</Menu.Item>
+              </Link>
+              <Link to="/signup">
+                <Menu.Item as="a">Sign Up</Menu.Item>
+              </Link>
+              <Menu.Item as="a">
+                <Link to="/cart">Cart</Link>
+              </Menu.Item>
+            </Container>
+          )}
+        </Menu>
+        <hr />
+      </div>
+    </nav>
+  )
+}
 
 /**
  * CONTAINER
  */
 const mapState = state => {
   return {
-        user: state.loginUser,
-        isLoggedIn: !!state.loginUser.id
+    user: state.loginUser,
+    isLoggedIn: !!state.loginUser.id
   }
 }
 
